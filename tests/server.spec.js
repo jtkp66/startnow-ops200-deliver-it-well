bed46dd61aa30644023df364f26daf88019ba9cd
Jamie Pham <jtkp66@gmail.com>
2018-04-17 20:30:16 -0700

    rdy to do something

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,15 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('page says hi world', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain('hi world');
+        done();
+      });
+    });
+
 })
